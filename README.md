942a54eb188f5b9b39776d92d294e1f820a786f0
farrari-100 <devendrakumarjnv6@gmail.com>
2024-07-23 20:27:01 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# JoyfulTunes
+
+JoyfulTunes is a music player. It provides an intuitive interface for users to play, pause, and navigate through their favorite tracks. The project features a responsive design, ensuring a seamless experience across different devices.
+
+## Features
+
+- Play, pause, and navigate through songs
+- Display current playing song details
+- Responsive design for mobile and desktop
+- Dark theme with special effects
+- Interactive progress bar
+- Music list display
+- Navbar with links to GitHub and LinkedIn
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+
+
+## Demo
+Check out the live demo [here](https://navodayanvinu.github.io/JoyfulTunes/).
+
+
+## Contact
+
+Vinod Malviya - [vk.malviya2002@gmail.com](vk.malviya2002@gmail.com)
+
+Project Link: [https://github.com/Navodayanvinu/JoyfulTunes](https://github.com/Navodayanvinu/JoyfulTunes)
---
+++ js/music-list.js
@@ -0,0 +1,189 @@
+let allMusic = [
+
+  {
+    name: "Dekha Tenu Pehli Pehli Baar Ve",
+    artist: "	Mohammad Faiz",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "	O Mahi O Mahi",
+    artist: "Arijit Singh",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "	Heeriye",
+    artist: "Jasleen Royal, Arijit Singh,",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "O Sajni Re",
+    artist: "Arijit Singh",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Chaleya",
+    artist: "Arijit Singh",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Besharam Rang",
+    artist: "	Shilpa Rao,Caralisa Monteiro",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Bhairava Anthem",
+    artist: "	Vijaynarain, Diljit Dosanjh",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Soulmate",
+    artist: "	Badshah, Arijit Singh",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Teri Galliyon Se",
+    artist: "	Jubin Nautiyal",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Maan Meri Jaan",
+    artist: "	King",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Barsaat Ho Jaaye",
+    artist: "Jubin Nautiyal, Payal Dev ,",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Enna Sona",
+    artist: "	Mitraz,Arooh,",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Noor Hi Noor ",
+    artist: "Raj Barman,",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Tum Se ",
+    artist: "	Sachin Jigar, Raghav Chaitanya,",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Teri Baaton Mein Aisa Uljha Jiya",
+    artist: "Raghav, Asees Kaur,",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Laal Peeli Akhiyaan",
+    artist: "	Romy, Tanishk Bagchi",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Pehle Bhi Main",
+    artist: "Vishal Mishra",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Lal Joda Pahne Aana ",
+    artist: "	Arijit Singh",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Meri Jeet Bhi Leja Meri Haar Bhi Leja",
+    artist: "	Justh,",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Soseki Pushpa2",
+    artist: "	Shreya Ghoshal",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "Jab Bhi Teri Yaad ",
+    artist: "	I SHOJ",
+    img: "music-21",
+    src: "music-21"
+  },
+  {
+    name: "Jab Bhi Teri Yaad ",
+    artist: "	I SHOJ",
+    img: "music-21",
+    src: "music-21"
+  },
+  {
+    name: "Premika Ne Pyar Se",
+    artist: "Udit Narayan",
+    img: "music-22",
+    src: "music-22"
+  },
+  {
+    name: "Tu Hai Gentleman Hum Hai Balak Desi",
+    artist: "	Addy Nagar",
+    img: "music-23",
+    src: "music-23"
+  },
+  {
+    name: "Saari Duniya Jala Denge",
+    artist: "	B Praak,",
+    img: "music-24",
+    src: "music-24"
+  },
+  {
+    name: "Jamal Kudu Remix - DJ",
+    artist: "	Harshavardhan Rameshwar",
+    img: "music-25",
+    src: "music-25"
+  },
+  {
+    name: "Pardesiya Yeh Sach Hai Piya Circuit Mix - DJ",
+    artist: "Kishore Kumar,",
+    img: "music-26",
+    src: "music-26"
+  },
+  {
+    name: "Ram Siya Ram",
+    artist: "Sachet Tandon, Parampara Tandon,",
+    img: "music-27",
+    src: "music-27"
+  },
+  {
+    name: "Manike",
+    artist: "Yohani,Jubin Nautiyal,",
+    img: "music-28",
+    src: "music-28"
+  },
+  {
+    name: "Sabki Baaratein Aayi 2",
+    artist: "	Dev Negi, Seepi Jha,",
+    img: "music-29",
+    src: "music-29"
+  },
+  {
+    name: "Tu Hi Chahiye",
+    artist: "	Ashwani Machal,",
+    img: "music-30",
+    src: "music-30"
+  },
+];
